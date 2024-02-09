dcef45f10c9b4400a69a23a270800d4da91df95e
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-02-09 20:37:06 +0600

    icons added

--- icons/index.ts
+++ icons/index.ts
@@ -1,4 +1,12 @@
 export { default as GoogleIcon } from './google-icon.svg';
 export { default as CaretSortIcon } from './caret-sort-icon.svg';
 export { default as ArrowRightIcon } from './arrow-right-icon.svg';
-export { default as ArrowLeftIcon } from './arrow-left-icon.svg'; 
+
+
+export { default as QrScanIcon } from './qr-scan-icon.svg';
+export { default as RadioCircleCheckedIcon } from './radio-circle-checked-icon.svg';
+export { default as ReceiptTextIcon } from './receipt-text-icon.svg';
+export { default as RightChevronIcon } from './right-chevron-icon.svg';
+export { default as SparkleIcon } from './sparkle-icon.svg';
+export { default as CardAddIcon } from './card-add-icon.svg';
+export { default as EditPencilIcon } from './edit-pencil-icon.svg'; 
